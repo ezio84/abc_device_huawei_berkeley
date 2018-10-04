aabf036ce0bbdb67347afdb3f070dfe84691a2bf
ezio84 <brabus84@gmail.com>
2018-10-04 15:27:40 +0200

    Drop apn-conf, we are using a prebuilt one from vendor/nexus

--- aosp_berkeley.mk
+++ aosp_berkeley.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 #
 
-PRODUCT_COPY_FILES += \
-    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+#PRODUCT_COPY_FILES += \
+    #device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 $(call inherit-product, device/huawei/berkeley/full_berkeley.mk)
 
